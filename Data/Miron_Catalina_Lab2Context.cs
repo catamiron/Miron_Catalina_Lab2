e0f513d07a33e27270ec2bcabe6bcb49823fc919
miron <miron@FELIXFELICIS>
2022-11-13 22:28:50 +0200

    Add project files.

---
+++ Data/Miron_Catalina_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Miron_Catalina_Lab2.Models;
+
+namespace Miron_Catalina_Lab2.Data
+{
+    public class Miron_Catalina_Lab2Context : DbContext
+    {
+        public Miron_Catalina_Lab2Context (DbContextOptions<Miron_Catalina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Miron_Catalina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Miron_Catalina_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
